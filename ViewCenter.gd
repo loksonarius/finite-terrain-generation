5c61c709d56d0ff2a421398ab4991a1321641528
Dan Herrera <sonarius@shew.io>
2020-06-12 22:30:34 -0400

    Implement mouse-based camera for test scene

--- ViewCenter.gd
+++ ViewCenter.gd
@@ -1,21 +1,29 @@
 extends Node2D
 
-const SPEED = 20
-const SHIFT_MULT = 10
+const ZOOM_FACTOR = 1.1
+var _zoom := Vector2.ONE
+onready var Camera = $Camera
+
+func _input(event: InputEvent):
+	if event is InputEventMouse:
+		global_position = event.global_position
+		if event.is_pressed():
+			match event.button_index:
+				BUTTON_WHEEL_UP:
+					if _zoom.x > 0.1:
+						_zoom /= ZOOM_FACTOR
+				BUTTON_WHEEL_DOWN:
+					if _zoom.x < 9.0:
+						_zoom *= ZOOM_FACTOR
+			Camera.zoom = _zoom
+	if event is InputEventAction:
+		if Input.is_action_just_pressed("reload"):
+			_center_cam()
+
 
 func _ready() -> void:
 	_center_cam()
 
-func _process(_delta) -> void:
-	if Input.is_action_just_pressed("reload"):
-		_center_cam()
-
-	var dx := Input.get_action_strength("right") - Input.get_action_strength("left")
-	var dy := Input.get_action_strength("down") - Input.get_action_strength("up")
-	var dis := Vector2(dx, dy).normalized() * SPEED
-	if Input.is_action_pressed("shift"):
-		dis *= SHIFT_MULT
-	translate(dis)
 
 func _center_cam() -> void:
 	var center := get_viewport().size * 0.5
